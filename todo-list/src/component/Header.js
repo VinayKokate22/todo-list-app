7796f757ef087b6bced95679a0db1b667cdaef6c
Vinay Kokate <vinaynkokate@gmail.com>
2023-01-22 02:37:54 +0530

    beta verson ready

---
+++ todo-list/src/component/Header.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <header>
+        <img src="" alt="" />
+        <h2>DoddleList</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil animi velit vitae a consectetur. Voluptate perspiciatis fugiat laudantium iste magni doloribus, nihil animi, suscipit eos doloremque repellendus sapiente veniam nulla?</p>
+    </header>
+    </>
+  )
+}
+
+export default Header
---
+++ todo-list/src/component/Todolist.js
@@ -0,0 +1,54 @@
+import React from 'react'
+import { useState } from 'react'
+
+const Todolist = () => {
+  const [prev,setprev]=useState("")
+  const [listData ,setlistData]=useState([])
+  function addActivity(){
+        setlistData((listData)=>{
+          const updateList=[...listData,prev]
+          setprev('')
+          return updateList
+        })
+  }
+  function removelist(listindex){
+        const updatedlistData= listData.filter((elem,id)=>{
+          return listindex!==id
+        })  
+        setlistData(updatedlistData)
+  }
+  return (
+   <>
+     <main className='background-color'>
+         <section className='todolistform'>
+            <img src="" alt="" />
+            <div className='tittle'>
+                <h2>To Do List</h2>
+            </div>
+            <div className='input-flex'>
+              <input type="text" placeholder='what to touch the grass' value={prev} onChange={(e)=>setprev(e.target.value)
+               }/>
+              <button className='addtask-button btn' onClick={addActivity}>Add Task</button>
+            </div>
+            
+             <div className='scroll-in-y'>
+
+                   <p className='taskremaining'>remaining Task -- keep it up !!</p>
+                  {listData!==[] && listData.map((data,i)=>{
+                    return(
+                      <>
+                      <p key={i} className='task-flex'>
+                        <div className='taskentered'>{data}</div>
+                        <button className='task-removebutton btn' onClick={()=>removelist(i)}>Done</button>
+                      </p>
+                      </>
+                    )
+                  })}
+             </div>
+         </section>
+     </main>
+   </>
+  )
+}
+
+export default Todolist
